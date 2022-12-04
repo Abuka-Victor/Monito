037d7fb6254c1cac0dbdbe87fbc5b33a095901ab
Abuka-Victor <abukavictoro@gmail.com>
2022-12-04 09:16:20 +0100

    Update backend url endpoint

--- src/frontend/src/config.js
+++ src/frontend/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  url: process.env.URLNAME || 'http://localhost:5500',
+  url2: process.env.URLNAME || 'http://localhost:5500',
+  url: 'https://monito-backend.onrender.com',
 };
 
 export default config;
